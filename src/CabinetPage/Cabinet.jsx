6bfc35f7c1061e731c2d361d4bc4718adf174c0d
zhansvltan <assemzhar@icloud.com>
2023-12-25 03:38:04 +0600

    some mobile adaptation

--- src/CabinetPage/Cabinet.jsx
+++ src/CabinetPage/Cabinet.jsx
@@ -1,8 +1,14 @@
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import ParkCard from '../components/ParkCard.jsx'
 
 const CabinetPage = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen)
+  }
   const cardsData = [
     {
       imageSrc: 'assets/park1.svg',
@@ -25,71 +31,76 @@ const CabinetPage = () => {
   ]
 
   return (
-    <div className="flex overflow-hidden">
+    <div className="flex overflow-hidden mobile:justify-center laptop:justify-start">
       <div
         id="sidebar"
-        className="flex-col justify-around bg-[#f0f0f0] rounded-[24px] mobile:hidden laptop:block laptop:w-[200px] laptop:min-h-[100vh] text-[18px]"
+        className={`flex-col justify-around bg-[#f0f0f0] rounded-[24px] ${
+          isSidebarOpen
+            ? 'mobile:block mobile:absolute mobile:left-0 mobile:pt-[20%] mobile:w-[170px] mobile:h-[100%]'
+            : 'mobile:hidden'
+        } laptop:static laptop:block laptop:w-[200px] laptop:min-h-[100vh] text-[18px]`}
       >
         <div className="flex-col h-[88%]" id="top side">
-          <div className="pt-8  pl-8">
+          <div className="pt-8  mobile:pl-4 laptop:pl-8">
             <img
               src="/assets/logo.svg"
               alt="logo"
-              className="w-[105px] h-[85px] mb-10 "
+              className="w-[105px] h-[85px] mb-10 mobile:hidden laptop:block"
             />
           </div>
-          <div className="flex gap-4 mb-3  pl-8 items-center">
+          <div className="flex gap-4 mb-3  mobile:pl-4 laptop:pl-8 items-center">
             <img
               src="/assets/home.svg"
               alt="home"
-              className="h-[20px] w-[20px]"
+              className="mobile:h-[16px] mobile:w-[16px] laptop:h-[20px] laptop:w-[20px]"
             />
-            <span>
+            <span className="font-[600] text-[#030303] mobile:text-[12px] laptop:text-[18px]">
               <Link to="/">Главная</Link>
             </span>
           </div>
           <div
-            className="flex gap-4 mb-3 bg-white rounded-[24px] pl-8 h-[44px] items-center"
+            className="flex gap-4 mb-3 bg-white rounded-[24px] mobile:pl-4 laptop:pl-8 h-[44px] items-center"
             style={{ boxShadow: '0px 2px 10px rgba(3,3,3,0.1)' }}
           >
             <img
               src="/assets/search.svg"
               alt="home"
-              className="h-[20px] w-[20px]"
+              className="mobile:h-[16px] mobile:w-[16px] laptop:h-[20px] laptop:w-[20px]"
             />
-            <span>
+            <span className="font-[600] text-[#030303] mobile:text-[12px] laptop:text-[18px]">
               <Link to="/">Поиск</Link>
             </span>
           </div>
-          <div className="flex gap-4 mb-16  pl-8 items-center">
+          <div className="flex gap-4 mb-16  mobile:pl-4 laptop:pl-8 items-center">
             <img
               src="/assets/bookmark.svg"
               alt="home"
-              className="h-[20px] w-[20px]"
+              className="mobile:h-[16px] mobile:w-[16px] laptop:h-[20px] laptop:w-[20px]"
             />
-            <span>
+            <span className="font-[600] text-[#030303] mobile:text-[12px] laptop:text-[18px]">
               <Link to="/">Парки</Link>
             </span>
           </div>
-          <div className="flex gap-4  pl-8 items-center">
+          <div className="flex gap-4  mobile:pl-4 laptop:pl-8 items-center">
             <img
               src="/assets/plus.svg"
               alt="home"
-              className="h-[20px] w-[20px]"
+              className="mobile:h-[16px] mobile:w-[16px] laptop:h-[20px] laptop:w-[20px]"
             />
-            <span>
+            <span className="font-[600] text-[#030303] mobile:text-[12px] laptop:text-[18px]">
               <Link to="/">Добавить</Link>
             </span>
           </div>
         </div>
-        <div className="flex-col  pl-8 " id="bottom side">
+
+        <div className="flex-col mobile:pl-4 laptop:pl-8 " id="bottom side">
           <div className="flex gap-4 mb-6 items-center">
             <img
               src="/assets/settings.svg"
               alt="home"
-              className="h-[20px] w-[20px]"
+              className="mobile:h-[16px] mobile:w-[16px] laptop:h-[20px] laptop:w-[20px]"
             />
-            <span>
+            <span className="font-[600] text-[#030303] mobile:text-[12px] laptop:text-[18px]">
               <Link to="/">Настройки</Link>
             </span>
           </div>
@@ -97,21 +108,34 @@ const CabinetPage = () => {
             <img
               src="/assets/logout.svg"
               alt="home"
-              className="h-[20px] w-[20px]"
+              className="mobile:h-[16px] mobile:w-[16px] laptop:h-[20px] laptop:w-[20px]"
             />
-            <span>
+            <span className="font-[600] text-[#030303] mobile:text-[12px] laptop:text-[18px]">
               <Link to="/">Выйти</Link>
             </span>
           </div>
         </div>
       </div>
-      <div className="flex flex-col  gap-[13vh] w-[85%]">
-        <div className="flex gap-[27vw] items-center pt-8 px-8 ">
-          <p className="text-[20px] text-[#030303] font-[600] leading-[32px] w-[10%]">
+      <div className="flex flex-col mobile:gap-[5vh] laptop:gap-[13vh] w-[85%]">
+        <div className="flex gap-[27vw] items-center laptop:pt-8 laptop:px-8">
+          <button
+            onClick={toggleSidebar}
+            className="mobile:absolute mobile:z-10 mobile:top-0 mobile:left-0 mobile:mobile:pl-4 laptop:pl-8 mobile:pt-6 mobile:block laptop:hidden"
+          >
+            <img
+              src="/assets/logo.svg"
+              alt="logo"
+              className="mobile:w-[60px] mobile:h-[50px] laptop:w-[105px] laptop:h-[85px] mb-10 "
+            />
+          </button>
+          <p className="text-[20px] text-[#030303] font-[600] leading-[32px] w-[10%] mobile:hidden laptop:block">
             Главная
           </p>
-          <div className="flex gap-10 items-center">
-            <div id="search" className="flex gap-4 items-center">
+          <div className="flex laptop:gap-10 items-center">
+            <div
+              id="search"
+              className="mobile:hidden laptop:block laptop:flex gap-4 items-center "
+            >
               <input
                 type="text"
                 placeholder="Введите наименование парка"
@@ -136,25 +160,29 @@ const CabinetPage = () => {
               <img
                 src="/assets/message.svg"
                 alt=""
-                className="w-[20px] h-[20px]"
+                className="w-[20px] h-[20px] mobile:hidden laptop:block"
               />
               <img
                 src="/assets/notify.svg"
-                className="w-[12px] h-[12px] absolute top-0 right-[-1px]"
+                className="w-[12px] h-[12px] absolute top-0 right-[-1px] mobile:hidden laptop:block"
               />
             </div>
             <div className="relative">
               <img
                 src="/assets/bell.svg"
                 alt=""
-                className="w-[20px] h-[20px]"
+                className="w-[20px] h-[20px] mobile:hidden laptop:block"
               />
               <img
                 src="/assets/notify.svg"
-                className="w-[12px] h-[12px] absolute top-0 right-[-1px]"
+                className="w-[12px] h-[12px] absolute top-0 right-[-1px] mobile:hidden laptop:block"
               />
             </div>
-            <img src="/assets/Image.svg" alt="" className="w-[48px] h-[48px]" />
+            <img
+              src="/assets/Image.svg"
+              alt=""
+              className="w-[48px] h-[48px] mobile:ml-[75vw] mobile:mt-6 laptop:ml-0 laptop:mt-0"
+            />
           </div>
         </div>
         <div
